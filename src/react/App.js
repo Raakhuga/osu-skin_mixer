fb96eaf06ca73d6dcc5d0ea4df89542b9946b567
Santi <santi.gstocks@gmail.com>
2020-05-14 16:18:02 +0200

    React + Electron Init

---
+++ src/react/App.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+import { channels } from '../shared/constants';
+const { ipcRenderer } = window;
+
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      appName: '',
+      appVersion: '',
+    };
+    ipcRenderer.send(channels.APP_INFO);
+    ipcRenderer.on(channels.APP_INFO, (event, arg) => {
+      ipcRenderer.removeAllListeners(channels.APP_INFO);
+      const { appName, appVersion } = arg;
+      this.setState({ appName, appVersion });
+    });
+  }
+
+  render() {
+    const { appName, appVersion } = this.state;
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <p>{appName} version {appVersion}</p>
+        </header>
+      </div>
+    );
+  }
+}
+
+/*
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edit <code>src/App.js</code> and save to reload.
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React
+        </a>
+      </header>
+    </div>
+  );
+}
+*/
+
+export default App;
